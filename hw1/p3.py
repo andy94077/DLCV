7af65cc1189c8031bbecaa60fab97b04d9ce3f95
andy94077 <andy94077@gmail.com>
2020-10-09 17:41:00 +0800

    Finish p3.py

---
+++ hw1/p3.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('lena.png', cv2.IMREAD_GRAYSCALE).astype(np.float64)
+
+# (1)
+blur = cv2.GaussianBlur(img, (3,3), 1 / (2 * np.log(2)))
+cv2.imwrite('3-1.png', blur)
+
+# (2)
+kx = np.array([[-0.5, 0.,0.5]])
+ky = np.array([[-0.5], [0], [0.5]])
+img_x=cv2.filter2D(img, -1, kx)
+img_y = cv2.filter2D(img, -1, ky)
+cv2.imwrite('3-2_x.png', np.where(img_x > 4, 0, 255).astype(np.uint8))
+cv2.imwrite('3-2_y.png', np.where(img_y > 4, 0, 255).astype(np.uint8))
+
+# (3)
+grad_img = np.sqrt(img_x ** 2 + img_y ** 2)
+grad_blur = np.sqrt(cv2.filter2D(blur, -1, kx)** 2 + cv2.filter2D(blur, -1, ky)** 2)
+
+cv2.imwrite('3-3_original.png', np.where(grad_img > 6, 0, 255).astype(np.uint8))
+cv2.imwrite('3-3_blur.png', np.where(grad_blur > 6, 0, 255).astype(np.uint8))
